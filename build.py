d909f27e90cc6fd93678f34cd1aa20509dc2ffe4
evhemsley <verdonhemsley@gmail.com>
2021-02-20 17:40:01 -0500

    created build.py to easily create a distribution

---
+++ build.py
@@ -0,0 +1,39 @@
+import os
+import PyInstaller.__main__
+
+
+NAME = 'dialogue-editor'
+VERSION = '0_0'
+
+README = \
+"""
+Run Instructions:
+-----------------
+run app.exe in /app folder
+
+
+Author:
+---------
+evhemsley
+"""
+
+PATH = f"./dist/{NAME}-{VERSION}"
+
+#builds project
+PyInstaller.__main__.run([
+    'main.py',
+    '--windowed',
+    '--noconfirm',
+    '--clean',
+    '--icon=build/flower_corner_image.ico',
+    '--distpath', PATH,
+    '--name', 'app'
+])
+
+#adds a readme
+with open(f"{PATH}/readme.txt", "w") as fh:
+    fh.write(README)
+
+#delete unused files
+if os.path.exists("app.spec"):
+    os.remove("app.spec")
